cc0e149c601fcbca3b6130fcbec2091b93fc72a2
Thales Nishida <medeiros.tn@gmail.com>
2024-08-14 08:39:07 -0300

    ### Fim aula 02 modulo 1 (Android com Kotlin: Migration e relacionamento com o Room) Destruindo Migration com Fallback

--- android/android-com-kotlin-migrations-e-relacionamento-com-room-projeto-inicial/app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
+++ android/android-com-kotlin-migrations-e-relacionamento-com-room-projeto-inicial/app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
@@ -16,7 +16,7 @@ import br.com.alura.orgs.model.Usuario
         Produto::class,
         Usuario::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = true
 )
 @TypeConverters(Converters::class)
